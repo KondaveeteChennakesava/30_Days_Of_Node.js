873e14fcd009c162d49dabc3524707a6b550c998
Chennak2248 <21A91A04L6@aec.edu.in>
2024-02-01 10:32:22 +0530

    Day-01 file reading asynchronously

---
+++ Day-01/fileReading.js
@@ -0,0 +1,14 @@
+const fs = require('fs').promises
+async function readFileContent(filepath){
+    try{
+        const data = await fs.readFile(filepath,'utf8');
+        console.log('File Content: ');
+        console.log(data);
+    }catch(err){
+        console.log(`Error reading file: , ${err}`);
+    }
+}
+
+readFileContent('test-cases/test.txt');
+readFileContent('test-cases/no-content.txt');
+readFileContent('test-cases/no-file.txt');
